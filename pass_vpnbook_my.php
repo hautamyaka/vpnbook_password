32b938374f9cab827b6d490134b90a8a61b067f2
Eugeniy Miropolskiy <19inch@mail.ru>
2022-04-16 23:19:59 +0300

    pass for vpnbook

---
+++ pass_vpnbook_my.php
@@ -0,0 +1,24 @@
+<?php
+function url_get_html($url) {
+    // инициализируем cURL
+    $ch = curl_init();
+    // устанавливаем url с которого будем получать данные
+    curl_setopt($ch, CURLOPT_URL, $url);
+    // устанавливаем опцию чтобы содержимое вернулось нам в string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // выполняем запрос
+    $output = curl_exec($ch);
+    // закрываем cURL
+    curl_close($ch);
+    // возвращаем содержимое
+    return $output;
+}
+
+$homepage = url_get_html('http://anapa-rg.ru/vpnbookpass.html');
+preg_match('/Password: ([^<]+)/', $homepage, $matches);
+
+// Print the entire match result
+// var_dump($matches);
+print($matches[1])
+// print_r($matches);
+?>
